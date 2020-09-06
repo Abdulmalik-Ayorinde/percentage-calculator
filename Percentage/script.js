fbdf420ea606e9e568accc4a68ddc0e557ba355b
Abdulmalik Ayorinde <55638353+Abdulmalik-Ayorinde@users.noreply.github.com>
2020-09-06 17:32:57 +0100

    All Functionalities add

--- Percentage/script.js
+++ Percentage/script.js
@@ -15,6 +15,8 @@ let mtn = [1000, 500, 1200, 1500, 2000, 2500, 3500, 20000, 30000, 50000, 75000]
 
 let glo = [500, 1000, 2000, 2500, 3000, 4000, 5000, 8000, 15000, 18000, 20000]
 
+console.log(glo)
+
 let airtel = [
 	1000,
 	1500,
@@ -32,10 +34,10 @@ let airtel = [
 let nmobile = [500, 1000, 1200, 2000, 5000, 10000, 15000, 27500]
 
 // Arrays of calculated percentage
-let mtnPercent = []
+let mtnCost = []
 let gloCost = []
-let airtelPercent = []
-let nmobilePercent = []
+let airtelCost = []
+let nmobileCost = []
 
 // Array for Earnings
 let mtnEarn = []
@@ -47,39 +49,25 @@ let nmobileEarn = []
 solution.addEventListener('click', () => {
 	calculateCard(glo, gloPrice, gloCost)
 	insertCard(gloCost)
+	console.log(gloCost)
 })
 
 addition.addEventListener('click', () => {
 	// let percentResult = percentage(mainPercent.value, mainPrice.value)
 	// console.log(Number(mainPrice.value) - Number(percentResult))
 
-	subtraction(glo, gloCost)
+	// subtraction(glo, gloCost)
+
+	calculateCost(glo, gloCost)
 })
 
-// const subtract = (arrMain, percentArr) => {
-// 	arrMain.forEach((card) => {
-// 		console.log('main ' + card)
-// 		percentArr.forEach((item) => {
-// 			const result = card - item
-// 			console.log('second ' + item)
-// 		})
-// 	})
-// }
+// Calculate Discounted price
 
-function arrMain() {
-	glo.forEach((card) => {
-		return card
+function calculateCost(mainArr, discountArr) {
+	var x = mainArr.map((item, index) => {
+		return item - discountArr[index]
 	})
-}
-
-function percentArr() {
-	gloCost.forEach((item) => {
-		return item
-	})
-}
-
-function subtraction(arrMain, percentArr) {
-	console.log(arrMain - percentArr)
+	console.log(x)
 }
 
 // percentage calculation function
@@ -87,6 +75,7 @@ function percentage(percent, total) {
 	return ((percent / 100) * total).toFixed(0)
 }
 
+// Calculating Earnings
 const calculateCard = (arr, price, newArr) => {
 	arr.forEach((item) => {
 		let percentResult = percentage(price, item)
